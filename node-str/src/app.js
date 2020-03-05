df3629673b2590fe348668076a41c869333c3a39
Higor barbosa <higorpereirabarbosa@gmail.com>
2020-03-04 21:45:09 -0300

    controlers m

--- node-str/src/app.js
+++ node-str/src/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const mongoose = require('mongoose');
 
 const app = express();
 
 const router = express.Router();
 
+mongoose.connect('mongodb+srv://higor:<h7654321>@cluster0-fhkdw.azure.mongodb.net/test?retryWrites=true&w=majority/',{useNewUrlParser: true,  useUnifiedTopology: true});
+console.log(`foiii`);
 //carrega rotas 
 const index = require(`./routes/index`);
 const products = require(`./routes/products`);
--- node-str/src/controlers/productsControler.js
+++ node-str/src/controlers/productsControler.js
@@ -1,13 +1,15 @@
-exports.post('/',(req,res,next) =>{ //rota padrao
+exports.post=('/',(req,res,next) =>{ //rota padrao
     res.status(201).send(req.body);
 });
 
-exports.delete('/:id',(req,res,next) =>{ //rota padrao
+exports.delete=('/:id',(req,res,next) =>{ //rota padrao
     let id = req.params.id;
     res.status(200).send({item :req.body, id: id});
 });
 
-exports.put('/:id',(req,res,next) =>{ //rota padrao
+exports.put=('/:id',(req,res,next) =>{ //rota padrao
     let id = req.params.id;
     res.status(201).send({item :req.body, id: id});
 });
+
+//mongodb+srv://higor:<password>@cluster0-fhkdw.azure.mongodb.net/test?retryWrites=true&w=majority
