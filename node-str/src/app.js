ea4600c48974c6163900d986d7de67a8e74e68f3
Higor barbosa <higorpereirabarbosa@gmail.com>
2020-03-06 16:19:05 -0300

    conectado ao mongo e primeiro model

--- node-str/src/app.js
+++ node-str/src/app.js
@@ -1,13 +1,28 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
 const mongoose = require('mongoose');
+const uri = "mongodb+srv://higor:<h7654321>@cursonode-d79zs.azure.mongodb.net/test?retryWrites=true&w=majority";
+
+mongoose.connect('mongodb://localhost/my_database', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+// const MongoClient = require('mongodb').MongoClient;
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true  });
+// client.connect(err => {
+//     console.log('conectado com sucesso ao mongo')
+//  // const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//  // client.close();
+// });
 
 const app = express();
 
 const router = express.Router();
 
-mongoose.connect('mongodb+srv://higor:<h7654321>@cluster0-fhkdw.azure.mongodb.net/test?retryWrites=true&w=majority/',{useNewUrlParser: true,  useUnifiedTopology: true});
-console.log(`foiii`);
+
 //carrega rotas 
 const index = require(`./routes/index`);
 const products = require(`./routes/products`);
